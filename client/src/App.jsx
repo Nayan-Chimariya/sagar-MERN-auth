42720254deacbea82543c81a2c276ab1b80000e9
Th-Sagar <sagarthapa98122@gmail.com>
2024-03-04 07:51:06 +0545

    chores: setup client and api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+const Home = lazy(()=>import('./pages/Home'))
+const About = lazy(()=>import('./pages/About'))
+const Profile = lazy(()=>import('./pages/Profile'))
+const Signin = lazy(()=>import('./pages/Signin'))
+const Signup = lazy(()=>import('./pages/Signup'))
+const Header = lazy(()=>import('./components/Header'))
 
 const App = () => {
+  const lazyloading =()=>{
+    return(
+      <div>loading...</div>
+    )
+  }
   return (
-    <div>App</div>
+    <Router>
+     <Suspense fallback={lazyloading()}>
+      <Header/>
+
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+
+      </Routes>
+     </Suspense>
+    </Router>
   )
 }
 
